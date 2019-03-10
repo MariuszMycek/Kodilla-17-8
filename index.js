25de75f3f81ef82e0b81cf37869601569b546df5
Mariusz Mycek <m.mycek83@gmail.com>
2019-03-10 15:58:19 +0100

    Change some index.js code - file stream code optimalization

--- index.js
+++ index.js
@@ -3,27 +3,33 @@ var fs = require("fs");
 var path = require("path");
 
 var server = http.createServer();
+
 server.on("request", function(request, response) {
+  /* 
+  Function fileStream(streamedFileDirectory, mediaType, encoding) is used to stream files on request. 
+  parameters: 
+  streamedFileDirectory - path to DIRECTORY where is stored file which we want to send;
+  mediaType - proper media type for response header: Content-Type;
+  encoding - file encoding.
+  */
+  function fileStream(streamedFileDirectory, mediaType, encoding) {
+    var filePath = path.join(streamedFileDirectory, request.url);
+    var fileStream = fs.createReadStream(filePath, encoding);
+    response.writeHead(200, { "Content-Type": mediaType });
+    fileStream.pipe(response);
+  }
+
   if (request.method === "GET" && request.url === "/") {
     response.writeHead(200, { "Content-Type": "text/html" });
     fs.readFile("./index.html", "utf-8", function(err, data) {
       response.end(data);
     });
   } else if (request.url.match(".css$")) {
-    var cssPath = path.join("./css", request.url);
-    var fileStream = fs.createReadStream(cssPath, "utf-8");
-    response.writeHead(200, { "Content-Type": "text/css" });
-    fileStream.pipe(response);
+    fileStream("./css", "text/css", "utf-8");
   } else if (request.method === "GET" && request.url.match(".jpg$")) {
-    var imagePath = path.join("./img", request.url);
-    var fileStream = fs.createReadStream(imagePath);
-    response.writeHead(200, { "Content-Type": "image/jpg" });
-    fileStream.pipe(response);
+    fileStream("./img", "image/jpg");
   } else if (request.url.match(".js$")) {
-    var jsPath = path.join("./js", request.url);
-    var fileStream = fs.createReadStream(jsPath, "utf-8");
-    response.writeHead(200, { "Content-Type": "text/javascript" });
-    fileStream.pipe(response);
+    fileStream("./js", "text/javascript", "utf-8");
   } else {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     response.statusCode = 404;
